a2bf9d818677da2fb9fb3f1d046292187c355d91
ahmetfirat23 <ahmetfiratgamsiz287@gmail.com>
2021-08-05 13:43:50 +0300

    GameLogic+Ending

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const Player = (sign, moves, color, name) => {
+const Player = (sign, color, name) => {
   this.name = name;
   this.color = color;
   this.sign = sign;
-  this.moves = moves;
 
-  function makeMove(moveInfo) {}
+  let moves = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+  let moveCount = 0;
 
-  return { name, color, sign, moves, makeMove };
+  function makeMove(moveInfo, otherPlayer) {
+    moves[moveInfo[0]][moveInfo[1]] = 1;
+    displayController.showTurnInfo(otherPlayer);
+    moveCount++;
+    if (moveCount >= 3) {
+      return moves;
+    }
+  }
+
+  function resetMoves() {
+    moves = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+  }
+
+  return { name, color, sign, makeMove, resetMoves };
 };
 
 const GameController = () => {
@@ -56,19 +77,35 @@ const GameController = () => {
     "input[type='radio']"
   );
 
-  const playerOne = Player("X", [[], [], []], "blue");
-  const playerTwo = Player("O", [[], [], []], "red");
+  const playerOne = Player("X", "blue");
+  const playerTwo = Player("O", "red");
 
   let boardArray;
   let moveInfo;
+  let currentArray;
 
   let currentPlayer;
   let otherPlayer;
 
   function start() {
+    document
+      .querySelector("#start-button")
+      .addEventListener("click", onStartClick);
     playerOneColorButtons.forEach((element) =>
       element.addEventListener("change", setPlayerColor)
     );
+    document
+      .querySelector("#change-settings-button")
+      .addEventListener("click", () => {
+        window.location.reload(false);
+      });
+    document
+      .querySelector("#play-again-button")
+      .addEventListener("click", () => {
+        onStartClick();
+        playerOne.resetMoves();
+        playerTwo.resetMoves();
+      });
   }
 
   function setPlayerColor(event) {
@@ -83,6 +120,7 @@ const GameController = () => {
     namePlayers();
     displayController.showGame(playerOne, playerTwo);
     displayController.showTurnInfo(playerOne);
+    displayController.clearBoard();
     startGame();
   }
 
@@ -93,16 +131,83 @@ const GameController = () => {
   }
 
   function startGame() {
-    boardArray = [];
+    boardArray = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
     currentPlayer = playerOne;
     otherPlayer = playerTwo;
+    document.querySelectorAll(".game-square").forEach((gameSquare) => {
+      gameSquare.addEventListener("click", onBoardClick);
+    });
+  }
+
+  function onBoardClick(event) {
+    moveInfo = getMoveInfo(event.target);
+    if (boardArray[moveInfo[0]][moveInfo[1]] == 0) {
+      boardArray[moveInfo[0]][moveInfo[1]] = 1;
+      currentArray = currentPlayer.makeMove(moveInfo, otherPlayer);
+      displayController.displayMove(event.target.id, currentPlayer);
+      if (victoryCheck(currentArray)) {
+        endGame(currentPlayer);
+        return;
+      } else if (
+        boardArray.every((array) => array.every((number) => number == 1))
+      ) {
+        drawEndGame();
+        return;
+      }
+      console.log(boardArray);
+      nextPlayer();
+    }
+  }
+
+  function nextPlayer() {
+    let temp = currentPlayer;
+    currentPlayer = otherPlayer;
+    otherPlayer = temp;
   }
 
   function getMoveInfo(square) {
-    return square.id.substring(1).split("-");
+    return square.id
+      .substring(1)
+      .split("-")
+      .map((number) => Number(number - 1));
   }
 
-  return { start, onStartClick };
+  function victoryCheck(array) {
+    if (array) {
+      for (let k = 0; k < victoryConditions.length; k++) {
+        let victoryCondition = victoryConditions[k];
+        let match = 0;
+        for (let i = 0; i < 3; i++) {
+          for (let j = 0; j < 3; j++) {
+            if (victoryCondition[i][j] == 1) {
+              if (array[i][j] == 1) {
+                match++;
+              }
+            }
+          }
+        }
+        if (match >= 3) {
+          return true;
+        }
+      }
+      return false;
+    }
+    return false;
+  }
+
+  function endGame(player) {
+    displayController.showEnding(player);
+  }
+
+  function drawEndGame() {
+    displayController.showDrawEnding();
+  }
+
+  return { start };
 };
 
 const displayController = (() => {
@@ -112,22 +217,19 @@ const displayController = (() => {
     green: "#518e30",
     yellow: "#efb31d",
   };
-  
-  const playerOneIngame = document.querySelector(
-    "#player-one-ingame"
-  );
-  const playerTwoIngame = document.querySelector(
-    "#player-two-ingame"
-  );
+
+  const playerOneIngame = document.querySelector("#player-one-ingame");
+  const playerTwoIngame = document.querySelector("#player-two-ingame");
 
   const showGame = (playerOne, playerTwo) => {
     const playerOneInfo = document.querySelector(
-        "#player-one-ingame .player-info"
-      );
-      const playerTwoInfo = document.querySelector(
-        "#player-two-ingame .player-info"
-      );
+      "#player-one-ingame .player-info"
+    );
+    const playerTwoInfo = document.querySelector(
+      "#player-two-ingame .player-info"
+    );
     document.querySelector("#login-screen").style.display = "none";
+    document.querySelector("#ending-screen").style.display = "none";
     document.querySelector("#game-display").style.display = "flex";
     playerOneInfo.style.color = colors[playerOne.color];
     playerTwoInfo.style.color = colors[playerTwo.color];
@@ -140,33 +242,65 @@ const displayController = (() => {
     playerOneInfo.firstElementChild.textContent =
       playerOneInfo.firstElementChild.textContent.replace(
         "Player One",
-        playerOne.name.substring(0,15)
+        playerOne.name.substring(0, 15)
       );
     playerTwoInfo.firstElementChild.textContent =
       playerTwoInfo.firstElementChild.textContent.replace(
         "Player Two",
-        playerTwo.name.substring(0,15)
+        playerTwo.name.substring(0, 15)
       );
   };
 
   const showTurnInfo = (player) => {
-    if(player.sign == 'X'){
-        playerOneIngame.lastElementChild.style.visibility = "visible";
-        playerTwoIngame.lastElementChild.style.visibility = "hidden";
-        console.log()
-    }
-    else{
-        playerOneIngame.lastElementChild.visibility = "hidden";
-        playerTwoIngame.lastElementChild.visibility = "visible";
+    if (player.sign == "X") {
+      playerOneIngame.lastElementChild.style.visibility = "visible";
+      playerTwoIngame.lastElementChild.style.visibility = "hidden";
+      console.log();
+    } else {
+      playerOneIngame.lastElementChild.style.visibility = "hidden";
+      playerTwoIngame.lastElementChild.style.visibility = "visible";
     }
   };
 
-  return { showGame, showTurnInfo };
+  const clearBoard = () => {
+    document.querySelectorAll(".game-square").forEach((gameSquare) => {
+      gameSquare.textContent = "";
+    });
+  };
+
+  const displayMove = (moveId, player) => {
+    document.querySelector(`#${moveId}`).textContent = player.sign;
+  };
+
+  const showEnding = (player) => {
+    let winnerPlayer = document.querySelector("#winner-player");
+    document.querySelector("#game-display").style.display = "none";
+    document.querySelector("#ending-screen").style.display = "flex";
+    winnerPlayer.style.color = player.color;
+    winnerPlayer.querySelector("#win-message").textContent = "Wins!";
+    winnerPlayer.querySelector("#win-message").style.fontSize = 96;
+    winnerPlayer.firstElementChild.setAttribute("src", `${player.color}.png`);
+    winnerPlayer.querySelector("#winner-name").textContent = player.name;
+  };
+
+  const showDrawEnding = () => {
+    document.querySelector("#game-display").style.display = "none";
+    document.querySelector("#ending-screen").style.display = "flex";
+    document.querySelector("#win-message").textContent = "It's a draw!";
+    document.querySelector("#win-message").style.fontSize = 108;
+    document.querySelector("#winner-player img").setAttribute("src","");
+    document.querySelector("#winner-name").textContent = "";
+  };
+
+  return {
+    showGame,
+    showTurnInfo,
+    clearBoard,
+    displayMove,
+    showEnding,
+    showDrawEnding,
+  };
 })();
+
 const gc = GameController();
-
-document
-  .querySelector("#start-button")
-  .addEventListener("click", gc.onStartClick);
-
 gc.start();
